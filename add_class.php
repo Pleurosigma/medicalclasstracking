51b241a3776e3276c9e0fbcd0ef5cea753500b36
Logan Wilkerson <Logan Wilkerson@localhost>
2010-10-27 16:57:31 -0400

    new add_class.php

--- add_class.php
+++ add_class.php
@@ -10,9 +10,17 @@ A page to add a class into the database
 	<h2>Add Class Page</h2>
 	<form name="classForm" action="class_ver.php" onsubmit = 'return validate_form( this )' method="post">
 	Class Name: <input type="text" name="classname" title="Class Name" /> </br>
-	Date (yyyy-mm-dd): <input type="text" name="date" title="Class Date" /> </br>
-	Start Time (hh:mm): <input type="text" name="starttime" title="Start time" /> </br>
-	End Time (hh:mm): <input type="text" name="endtime" title="End Time" /> </br>
+	Date: <input type="text" name="year" title="Year" value="yyyy"/> 
+		<input type="text" name="month" title="Month" value="mm"/> 
+		<input type="text" name="day" title="Day" value="dd" /> </br>
+	Start Time: <input type="text" name="starttimehr" title="Start time Hour" value="hh" />
+			<input type="text" name="starttimemin" title="Start Time Minute" value="mm" />
+		<input type="radio" name="amstart" value=True /> am
+		<input type="radio" name="amstart" value=False /> pm</br>
+	End Time: <input type="text" name="endtimehr" title="End Time Hour" value = "hh" /> 
+			<input type="text" name="endtimemin" title="End Time Minute" value="mm" />
+		<input type="radio" name="amend" value=True /> am
+		<input type="radio" name="amend" value=False /> pm</br>
 	Credits: <input type="text" name="credits" title="Credits" /> </br>
 	Faculty: <input type="text" name="faculty" title="Faculty" /> </br>	
 	<input type="submit" value="Enter" />
